39ce9318d409a44cd6424cdf1b3ff2e203afca59
Sachintha Nirmal <s.nirmal.sm@gmail.com>
2025-04-28 12:19:14 +0530

    Let to share a post for a group

--- backend/src/main/java/com/university/skillshare_backend/model/GroupPost.java
+++ backend/src/main/java/com/university/skillshare_backend/model/GroupPost.java
@@ -19,3 +19,28 @@ public class GroupPost {
         this.sharedAt = new Date();
     }
 }
+
+
+
+/*package com.university.skillshare_backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+import java.util.Date;
+
+@Data
+@Document(collection = "group_posts")
+public class GroupPost {
+    @Id
+    private String id;
+    private String groupId;
+    private String postId;
+    private String sharedById;
+    private Date sharedAt;
+    
+    public GroupPost() {
+        this.sharedAt = new Date();
+    }
+}
+ */
